444fbcc42691697593b34f7a93f9b63b8d8fb782
Alberto Araujo <albertoig8@hotmail.com>
2024-06-13 16:43:13 -0300

    adicionando firestore nas configs do firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 // src/firebase.js
 import { initializeApp } from 'firebase/app';
 import { getAuth, signInWithCredential, GoogleAuthProvider } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore'; 
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -14,5 +15,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getFirestore(app);
 
-export { auth, signInWithCredential, GoogleAuthProvider };
+export { auth, signInWithCredential, GoogleAuthProvider, db };
